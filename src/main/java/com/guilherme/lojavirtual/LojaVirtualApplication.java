cddf4d1d721e4a9046999ccdace14d00afa74f3b
Guilherme Magalhaes <guilherme.magalhaes2907@gmail.com>
2019-01-10 19:44:32 -0200

    Instanciando e inserindo categorias no BD para teste

--- src/main/java/com/guilherme/lojavirtual/LojaVirtualApplication.java
+++ src/main/java/com/guilherme/lojavirtual/LojaVirtualApplication.java
@@ -1,14 +1,29 @@
 package com.guilherme.lojavirtual;
 
+import com.guilherme.lojavirtual.domain.Categoria;
+import com.guilherme.lojavirtual.repositories.CategoriaRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LojaVirtualApplication {
+public class LojaVirtualApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LojaVirtualApplication.class, args);
-	}
+    @Autowired
+    CategoriaRepository categoriaRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaVirtualApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+        
+        categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+    }
 
 }
-
